f7e5acc847fcf3a8db0890b15485f3004ccd2d41
Conley <71302026+conley21p@users.noreply.github.com>
2022-07-26 06:38:06 -0500

    Update ebayBidder.py

--- ebayBidder.py
+++ ebayBidder.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+'''
+Download chromeDriver from https://chromedriver.chromium.org/downloads
+'''
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 driver.maximize_window()
